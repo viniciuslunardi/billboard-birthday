37ffbed8c748f0f3b391b0a0fa5b1a66216b3292
viniciuslunardi <vinilunardi00@gmail.com>
2022-03-12 19:03:33 -0300

    fixing nock test

--- test/functional/billboard/billboard-birthday.test.ts
+++ test/functional/billboard/billboard-birthday.test.ts
@@ -8,16 +8,27 @@ describe('billboard tests', () => {
   const baseUrl = '/api/billboard-birthday/top-hundred';
 
   it('should be able to get billboard top 100', async () => {
-    const date = '2000-02-06';
-
-    nock.recorder.rec();
+    const date = '2020-02-20';
 
     nock('https://billboard-api2.p.rapidapi.com:443', {
       encodedQueryParams: true,
     })
       .get('/hot-100')
-      .query({ range: '1-1', date: '2000-02-06' })
-      .reply(OK, billboardTopHundredResponse, []);
+      .query({ range: '1-1', date: '2020-02-20' })
+      .reply(OK, billboardTopHundredResponse, [
+        'X-Powered-By',
+        'Express',
+        'Content-Type',
+        'application/json; charset=utf-8',
+        'Content-Length',
+        '129',
+        'ETag',
+        'W/"81-/JqAkCjjtrhbxcWJ6BV0JOxfE/s"',
+        'Date',
+        'Sat, 12 Mar 2022 21:45:56 GMT',
+        'Connection',
+        'close',
+      ]);
 
     const response = await global.testRequest.get(`${baseUrl}?date=${date}`);
     expect(response.status).toBe(OK);
